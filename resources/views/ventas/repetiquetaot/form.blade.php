1feda2a5a2ef3d72ae5d2908f3ffc03efc7a710e
Sergio Granucci <sergiogranucci@gmail.com>
2026-09-22 15:20:17 +0000

    Mejora en seteo de etiquetas de OT para ferli

--- resources/views/ventas/repetiquetaot/form.blade.php
+++ resources/views/ventas/repetiquetaot/form.blade.php
@@ -12,10 +12,13 @@
 			<select name="tipoetiqueta" id="tipoetiqueta" class="col-lg-3 form-control" required>
     			<option value="">-- Elija tipo de etiqueta --</option>
        			@foreach($tipoetiqueta_enum as $value => $tipoetiqueta)
-       				@if($value == 'CAJA FOTO')
-       					<option value="{{ $value }}" selected="select">{{ $tipoetiqueta }}</option>    
+					@php
+						$tipoSeleccionado = old('tipoetiqueta', 'CAJA FOTO');
+					@endphp
+       				@if($value == $tipoSeleccionado)
+       					<option value="{{ $value }}" selected="select">{{ $tipoetiqueta }}</option>
        				@else
-       					<option value="{{ $value }}">{{ $tipoetiqueta }}</option>    
+       					<option value="{{ $value }}">{{ $tipoetiqueta }}</option>
        				@endif
        			@endforeach
 			</select>
@@ -26,10 +29,13 @@
 			<select name="origen" class="col-lg-3 form-control" required>
 				<option value="">-- Elija origen --</option>
 				@foreach($origen_enum as $value => $origen)
-					@if( $value == 'ERP')
-						<option value="{{ $value }}" selected="select">{{ $origen }}</option>    
+					@php
+						$origenSeleccionado = old('origen', 'ERP');
+					@endphp
+					@if($value == $origenSeleccionado)
+						<option value="{{ $value }}" selected="select">{{ $origen }}</option>
 					@else
-						<option value="{{ $value }}">{{ $origen }}</option>    
+						<option value="{{ $value }}">{{ $origen }}</option>
 					@endif
 				@endforeach
 			</select>
